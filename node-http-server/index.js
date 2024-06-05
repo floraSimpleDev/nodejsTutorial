2943e0b19d188ce914f442e8517f9ca1f67625c6
Flora Li <samantha20200604@gmail.com>
2024-06-05 13:49:39 +1000

    tutorial 4.4 set up dynamic data query of guitars data

--- node-http-server/index.js
+++ node-http-server/index.js
@@ -1,13 +1,53 @@
 import { createServer } from "http";
+import guitars from "./data.js";
 
 const server = createServer((request, response) => {
-  /* response.write("Hello ");
-  response.end("World!"); */
+  /* 4.3 tutorial
+  response.write("Hello ");
+  response.end("World!"); 
 
   let content = "Hello Node.js!";
   response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  response.end(content);*/
+  response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  const url = new URL(request.url, "http://localhost");
+  const id = url.searchParams.get("id");
+
+  const content = `
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Guitars</title>
+    </head>
+    <body style="font-size: 2rem">
+        ${id ? getGuitarContent(id) : createList()}
+    </body>
+    </html>
+    `;
   response.end(content);
 });
+
+const createList = () => `
+  <h2>My Guitars</h2>
+  <ul>
+    ${guitars.map(createListItem).join("\n")}
+  </ul>`;
+
+const createListItem = ({ id, make, model }) =>
+  `<li>
+    <a href="?id=${id}">${make} ${model}</a>
+  </li>`;
+
+const getGuitarContent = (id) => {
+  const guitar = guitars.find((gui) => gui.id === id);
+
+  return guitar
+    ? `<h2>${guitar.make} ${guitar.model}</h2>`
+    : "<p>Guitar does not exist.</p>";
+};
 
 server.listen(80, () => {
   console.log(
